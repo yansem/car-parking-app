b2340e7e44fa37a79a9439f2cd9428183313a740
yansem <janchevsem@mail.ru>
2023-04-19 10:53:12 +0300

    register new user

---
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        event(new Registered($user));
+
+        $device = substr($request->userAgent() ?? '', 0, 255);
+
+        return response()->json([
+            'access_token' => $user->createToken($device)->plainTextToken,
+        ], Response::HTTP_CREATED);
+    }
+}
